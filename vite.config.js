4261f39fe81e9c87eaab4cfb2a03fcdf3e8cb702
reaper <ahoy@barelyhuman.dev>
2024-10-05 17:45:50 +0530

    chore: switch to the latest version of adex

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite'
 import { adex } from 'adex'
+import { providers } from 'adex/fonts'
 import preact from '@preact/preset-vite'
 
 export default defineConfig({
   build: {
-    target: 'node16',
+    target: 'node18',
   },
-  plugins: [adex(), preact()],
+  plugins: [
+    adex({
+      fonts: {
+        providers: [providers.bunny()],
+        families: [
+          {
+            name: 'Bad Script',
+            weights: ['400'],
+          },
+          {
+            name: 'Inter',
+            weights: ['400'],
+            styles: ['normal'],
+          },
+        ],
+      },
+    }),
+    preact(),
+  ],
 })
